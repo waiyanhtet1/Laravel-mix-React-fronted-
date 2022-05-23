5bed1ed092dc1e164a094d0b568c41248cf092a9
Wai_Yan_Htet <waiyanhtet1661390@gmail.com>
2022-05-23 16:04:54 +0630

    filter by category and load more page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,51 +5,82 @@ import Master from './Layout/Master'
 
 function Home() {
 
-  const[loader,setLoader] = useState(true)
+  const[pageLoader,setPageLoader] = useState(true)
+  const[productLoader,setProductLoader] = useState(false)
   const[product,setProduct] = useState({data:[]})
   const[category,setCategory] = useState({data:[]})
-  const[selectedCatgory,setSelectedCategory] = useState()
+  const[selectedCatgory,setSelectedCategory] = useState(null)
   const [currentPage,setCurrentPage] = useState(1)
   const [api,setApi] = useState('/product')
 
   useEffect(()=>{
     ax.get(api).then((res)=>{
-      setLoader(false)
+      setPageLoader(false)
+      setProductLoader(false)
       setProduct(res.data.data)
     })
 
     ax.get('/category').then((res)=>{
       setCategory(res.data.data)
+      setPageLoader(false)
     })
   },[api])
 
+  const viewAll = () => {
+    setSelectedCategory(null)
+    setCurrentPage(1)
+    setApi('/product')
+  }
+
+  const renderProductByCategory = (id) =>{
+    setProductLoader(true)
+    setCurrentPage(1)
+    setSelectedCategory(id)
+    setApi(`/product?category_id=${id}`)
+  }
+
   const renderNextPage = () => {
+    setProductLoader(true)
     setCurrentPage(currentPage + 1)
     const page = currentPage + 1
-    setApi(`/product?page=${page}`)
+    if(selectedCatgory===null){
+      setApi(`/product?page=${page}`)
+    } else {
+      setApi(`/product?category_id=${selectedCatgory}&page=${page}`)
+    }
   }
 
   const renderPrevPage = () => {
+    setProductLoader(true)
     setCurrentPage(currentPage - 1)
     const page = currentPage - 1
-    setApi(`/product?page=${page}`)
+    if(selectedCatgory===null){
+      setApi(`/product?page=${page}`)
+    } else {
+      setApi(`/product?category_id=${selectedCatgory}&page=${page}`)
+    }
   }
 
   return (
     <Master>
          {
-           loader ? <Spinner />
+           pageLoader ? <Spinner />
            :
           <>
             <div className="row mb-4">
               <div className="col-md-12">
+                <span 
+                className={selectedCatgory===null ? 'btn btn-black': 'btn btn-outline-black'} 
+                style={{marginRight:10}}
+                onClick={()=>viewAll()}>
+                  View All</span>
               {
                 category.data.map((d)=>(
                       <span 
                       className={selectedCatgory===d.id ? 'btn btn-black' : 'btn btn-outline-black'} 
                       style={{marginRight:10}}
                        key={d.id} 
-                      onClick={()=>setSelectedCategory(d.id)} >
+                      onClick={()=>renderProductByCategory(d.id)} >
                         {d.name}
                         </span>
                     ))
@@ -59,22 +90,25 @@ function Home() {
 
             <div className="row">
               {
-                product.data.map((d)=>(
-                  <div className="col-md-3" key={d.id}>
-                        <div className="card">
-                            <img src={`http://localhost:8000/images/${d.image}`} alt="" width='200px' />
-                            <div className="card-body">
-                                <h6 className='text-center'>{d.name}</h6>
-                                <div className="d-flex justify-content-between mt-4">
-                                    <span>$ {d.price}</span>
-                                    <button className='btn btn-sm btn-warning'>Add to Cart</button>
+                productLoader ? <Spinner />
+                :
+                <>
+                  {
+                    product.data.map((d)=>(
+                      <div className="col-md-3" key={d.id}>
+                            <div className="card">
+                                <img src={`http://localhost:8000/images/${d.image}`} alt="" width='200px' />
+                                <div className="card-body">
+                                    <h6 className='text-center'>{d.name}</h6>
+                                    <div className="d-flex justify-content-between mt-4">
+                                        <span>$ {d.price}</span>
+                                        <button className='btn btn-sm btn-warning'>Add to Cart</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                  </div> 
-                ))
-              }    
-            </div>
+                      </div> 
+                    ))
+                  }   
             <div className="text-center mt-5">
               <button className='btn btn-primary' 
               disabled={product.prev_page_url === null} onClick={()=>renderPrevPage()}>
@@ -84,6 +118,9 @@ function Home() {
               disabled={product.next_page_url === null} onClick={()=>renderNextPage()}>
                 <i className='fas fa-arrow-right'></i></button>
             </div>
+                </>
+              }
+          </div>
           </>
          }
     </Master>
